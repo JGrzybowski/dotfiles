85c1a5789e113da187dacb9056d87dd08de8a7d9
Jan Grzybowski <JanGrzybowski@outlook.com>
2019-09-09 15:41:53 +0200

    Fixed misspelled script name

--- WorkstationSetup/FullInstall.bat
+++ WorkstationSetup/FullInstall.bat
@@ -1,3 +1,3 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -NoLogo -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -NoLogo -InputFormat None -ExecutionPolicy Bypass -File "installFullSoftware.ps1" 
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -NoLogo -InputFormat None -ExecutionPolicy Bypass -File "installAllSoftware.ps1" 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -NoLogo -InputFormat None -ExecutionPolicy Bypass -File "autoUpgradableSoftware.ps1" 
