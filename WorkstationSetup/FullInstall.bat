3239dfd93b20c11d32b43f12c6c6b736c5d3084c
Jan Grzybowski <JanGrzybowski@outlook.com>
2021-02-07 17:37:07 +0100

    Updated FullInstall.bat

--- WorkstationSetup/FullInstall.bat
+++ WorkstationSetup/FullInstall.bat
@@ -1,4 +1,4 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -NoLogo -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -NoLogo -InputFormat None -ExecutionPolicy Bypass -File "autoUpgradableSoftware.ps1" 
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -NoLogo -InputFormat None -ExecutionPolicy Bypass -File "installAllSoftware.ps1" 
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -NoLogo -InputFormat None -ExecutionPolicy Bypass -File "software.ps1" 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -NoLogo -InputFormat None -ExecutionPolicy Bypass -File "Software\VSCode\installExtensions.ps1" 
--- WorkstationSetup/installAllSoftware.ps1
+++
@@ -1,8 +0,0 @@
-#Installs all software groups
-& "$PSScriptRoot\Software\communicators.ps1"
-& "$PSScriptRoot\Software\documents.ps1"
-& "$PSScriptRoot\Software\dev.ps1"
-& "$PSScriptRoot\Software\graphics.ps1"
-& "$PSScriptRoot\Software\maintenance.ps1"
-& "$PSScriptRoot\Software\tools.ps1"
-& "$PSScriptRoot\Software\video.ps1"
